e67e3045c70d557791ffa7ec45ea90b2f71256a9
Timur Kh. <timka@timka.su>
2026-04-13 21:10:54 +0300

    add missing migrations

---
+++ app/db/migrations/versions/5e176f8cb9bd_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 5e176f8cb9bd
+Revises: 5f4c2c762fce, b7c4d9e8f1a2
+Create Date: 2026-04-13 21:10:32.599279
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5e176f8cb9bd'
+down_revision = ('5f4c2c762fce', 'b7c4d9e8f1a2')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
